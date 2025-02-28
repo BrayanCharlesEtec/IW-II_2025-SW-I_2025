d867ac4605e3226268c8010b5172911abce6b7df
bryynnn <161508666+BrayanCharlesEtec@users.noreply.github.com>
2025-02-28 18:16:47 -0300

    Add files via upload

---
+++ aula03/processa01E.php
@@ -0,0 +1,8 @@
+<?php
+$nome = $_POST["cxnome"];
+$mensagem = $_POST["mensagem"];
+
+echo "<h2>Dados enviados:</h2>";
+echo "<p>Nome: " . $nome . "</p>";
+echo "<p>Mensagem: " . $mensagem . "</p>";
+?>
